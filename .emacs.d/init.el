e78b81300ad5a6d5d04ab82d066d94a6097280b5
Christopher Batey <christopher.batey@gmail.com>
2015-11-29 17:54:54 +0000

    Adding MELPA

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,20 @@
 (let ((default-directory "~/.emacs.d/lisp/"))
   (normal-top-level-add-subdirs-to-load-path))
 
-
 (require 'yaml-mode)
     (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
 (require 'go-mode-autoloads)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+
