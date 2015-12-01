813e6414d84556797e184d19e10e1728cc674736
Chris Batey <christopher.batey@sky.uk>
2015-12-01 14:52:29 +0000

    Install packages automatically

---
+++ .emacs.d/my-packages.el
@@ -0,0 +1,20 @@
+(defvar required-packages
+  '(
+    ensime
+    groovy
+    ) "A list of packages to install on startup)")
+
+(require 'cl)
+
+(defun packages-installed-p ()
+  (loop for p in required-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (packages-installed-p)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  (dolist (p required-packages)
+    (when (not (package-installed-p p))
+            (package-install p))))
