1868b1b6dd0b67fd6cba6444c0c97cd558c6f8b7
Christopher Batey <christopher.batey@gmail.com>
2016-02-16 21:38:48 +0000

    Set tad width to 2 for golang

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,12 @@
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 
+
+(defun my-go-mode-hook ()
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (setq tab-width 2 indent-tabs-mode 1))
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
